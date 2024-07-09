7b6121baad4902994474cd65c35af02f0a6644ae
Chris <19819051+Chrisdbhr@users.noreply.github.com>
2024-07-09 07:17:12 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+public class Program
+{
+    private static DiscordSocketClient _client;
+
+    public static async Task Main()
+    {
+        _client = new DiscordSocketClient();
+        _client.Log += Log;
+        var token = Environment.GetEnvironmentVariable("DISCORD_TOKEN_UTSUKI");
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+        await Task.Delay(-1);
+    }
+
+    private static Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
