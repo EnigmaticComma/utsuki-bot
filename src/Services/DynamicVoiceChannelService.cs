1743d1a6e8c387b2681674dbdae5e90b2ffdff14
Chris <19819051+Chrisdbhr@users.noreply.github.com>
2026-01-20 17:36:26 -0300

    dynamic voice channel fixed

--- src/Services/DynamicVoiceChannelService.cs
+++ src/Services/DynamicVoiceChannelService.cs
@@ -13,16 +13,14 @@ namespace App.Services;
 [Service]
 public class DynamicVoiceChannelService
 {
-    List<ulong> _dynamicCreatedVoiceChannels = new ();
-    ulong _mainVoiceChannelId = 822721840404889620;
-    SocketVoiceChannel _mainVoiceChannel;
+    // SocketVoiceChannel _mainVoiceChannel; // Unused - Removed
+
+    HashSet<ulong> _dynamicCreatedVoiceChannels = new();
 
     readonly DiscordSocketClient _discord;
     readonly LoggingService _log;
     readonly GuildSettingsService _guildSettingsService;
 
-    // Persist this list!
-    HashSet<ulong> _dynamicCreatedVoiceChannels = new();
     
     const string PERSISTENCE_PATH = "DynamicVoiceData/active_channels";
 
@@ -56,12 +54,91 @@ public class DynamicVoiceChannelService
         JsonCache.SaveToJson(PERSISTENCE_PATH, _dynamicCreatedVoiceChannels.ToList());
     }
 
-    async Task CleanupOrphans()
+
+
+    async Task OnUserVoiceStateUpdated(SocketUser user, SocketVoiceState previousVoiceState, SocketVoiceState newVoiceState)
+    {
+        ulong reusedChannelId = 0;
+        if (newVoiceState.VoiceChannel != null)
+        {
+           reusedChannelId = await HandleJoinedVoice(newVoiceState.VoiceChannel, user);
+        }
+        
+        if (previousVoiceState.VoiceChannel != null)
+        {
+           await HandleLeftVoice(previousVoiceState.VoiceChannel, reusedChannelId);
+        }
+    }
+
+    /// <returns>The ID of the reused channel, or 0 if created new or none.</returns>
+    async Task<ulong> HandleJoinedVoice(SocketVoiceChannel joinedChannel, SocketUser user)
+    {
+        var guildId = joinedChannel.Guild.Id;
+        var settings = _guildSettingsService.GetGuildSettings(guildId);
+        
+        // If this is the "Hub" channel
+        if (settings.DynamicVoiceSourceId.HasValue && joinedChannel.Id == settings.DynamicVoiceSourceId.Value)
+        {
+             // Check if we can reuse an empty dynamic channel
+             foreach(var id in _dynamicCreatedVoiceChannels)
+             {
+                 if (await _discord.GetChannelAsync(id) is SocketVoiceChannel vc)
+                 {
+                     if (!HasAnyUsersOnVoiceChannel(vc))
+                     {
+                         Console.WriteLine($"Reusing existing channel {vc.Name} ({vc.Id})");
+                          return vc.Id;
+                     }
+                 }
+             }
+
+             await CreateDynamicVoiceChannel(joinedChannel, user);
+        }
+        return 0;
+    }
+
+    async Task HandleLeftVoice(SocketVoiceChannel leftChannel, ulong reusedChannelId)
+    {
+        // 1. If this was a dynamic channel
+        if (_dynamicCreatedVoiceChannels.Contains(leftChannel.Id))
+        {
+            // If it's empty, delete it UNLESS it was just reused
+            if (!HasAnyUsersOnVoiceChannel(leftChannel))
+            {
+                if (leftChannel.Id == reusedChannelId)
+                {
+                    Console.WriteLine($"Skipping deletion of {leftChannel.Name} because it was reused.");
+                    return;
+                }
+                await DeleteDynamicChannel(leftChannel);
+            }
+            return;
+        }
+
+        // 2. If this was the Hub channel... (User left Hub)
+        // If they moved to a dynamic channel (reused or new), that dynamic channel is now occupied (or will be).
+        // If they left Hub to disconnect, we might have empty channels.
+        // We can run cleanup, but we must be careful not to delete the one we just moved them to.
+        var guildId = leftChannel.Guild.Id;
+        var settings = _guildSettingsService.GetGuildSettings(guildId);
+        
+        if (settings.DynamicVoiceSourceId.HasValue && leftChannel.Id == settings.DynamicVoiceSourceId.Value)
+        {
+             // If we reused a channel, we don't want CleanupOrphans to kill it before they arrive
+             // But CleanupOrphans checks ConnectedUsers. If the move is pending, it might be 0.
+             // We should pass the reused ID to CleanupOrphans to exempt it.
+             await CleanupOrphans(reusedChannelId);
+        }
+    }
+
+    async Task CleanupOrphans(ulong exemptId = 0)
     {
         // cleanup channels that might be totally empty after a restart
         var toRemove = new List<ulong>();
         foreach (var id in _dynamicCreatedVoiceChannels)
         {
+            if (id == exemptId) continue;
+
             var channel = await _discord.GetChannelAsync(id) as SocketVoiceChannel;
             if (channel == null)
             {
@@ -83,52 +160,9 @@ public class DynamicVoiceChannelService
         }
     }
 
-    async Task OnUserVoiceStateUpdated(SocketUser user, SocketVoiceState previousVoiceState, SocketVoiceState newVoiceState)
-    {
-        if (newVoiceState.VoiceChannel != null)
-        {
-           await HandleJoinedVoice(newVoiceState.VoiceChannel);
-        }
-        
-        if (previousVoiceState.VoiceChannel != null)
-        {
-           await HandleLeftVoice(previousVoiceState.VoiceChannel);
-        }
-    }
-
-    async Task HandleJoinedVoice(SocketVoiceChannel joinedChannel)
-    {
-        var guildId = joinedChannel.Guild.Id;
-        var settings = _guildSettingsService.GetGuildSettings(guildId);
-        
-        // If this is the "Hub" channel, create a new dynamic channel
-        if (settings.DynamicVoiceSourceId.HasValue && joinedChannel.Id == settings.DynamicVoiceSourceId.Value)
-        {
-             await CreateDynamicVoiceChannel(joinedChannel);
-        }
-    }
-
-    async Task HandleLeftVoice(SocketVoiceChannel leftChannel)
-    {
-        // If this was a dynamic channel and is now empty, delete it
-        if (_dynamicCreatedVoiceChannels.Contains(leftChannel.Id))
-        {
-            if (!HasAnyUsersOnVoiceChannel(leftChannel))
-            {
-                await DeleteDynamicChannel(leftChannel);
-            }
-        }
-    }
-
-    async Task CreateDynamicVoiceChannel(SocketVoiceChannel hubChannel)
+    async Task CreateDynamicVoiceChannel(SocketVoiceChannel hubChannel, SocketUser user)
     {
         var guild = hubChannel.Guild;
-        
-        // Anti-spam check: user just joined hub. 
-        // Logic: Create new channel, move user to it? 
-        // Original logic didn't move explicitly? Let's check original. 
-        // Original logic: "CreateMoreIfNecessary". It seemed to create one if empty ones were full.
-        // Usually these bots move the user. I'll add the MoveAsync logic for better UX.
         
         string suffix = "X";
         try {
@@ -148,30 +182,11 @@ public class DynamicVoiceChannelService
                 p.CategoryId = hubChannel.CategoryId;
                 p.PermissionOverwrites = new Optional<IEnumerable<Overwrite>>(hubChannel.PermissionOverwrites);
                 p.UserLimit = hubChannel.UserLimit;
-                // Position it below the hub?
+                p.Position = hubChannel.Position + 1;
             });
 
             _dynamicCreatedVoiceChannels.Add(newVc.Id);
             SaveState();
-            
-            // Move the user who joined the hub into the new channel
-            // We need to find *who* caused this. 
-            // In OnUserVoiceStateUpdated we don't have the user reference easily passed down without changing sig, 
-            // but we can iterate users in the Hub channel.
-            
-            // Wait a tiny bit for the user to be fully "in" (discord api quirks)
-            // But we are in an event handler, so joinedChannel.ConnectedUsers should be up to date or close.
-            
-            var users = hubChannel.ConnectedUsers.ToList();
-            foreach(var user in users)
-            {
-                try {
-                    await user.ModifyAsync(x => x.ChannelId = newVc.Id);
-                } catch(Exception e) {
-                   Console.WriteLine($"Failed to move user {user.Username}: {e.Message}");
-                }
-            }
-
             Console.WriteLine($"Created dynamic voice '{name}' in {guild.Name}");
         }
         catch (Exception e)
